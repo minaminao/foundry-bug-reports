60a9a01c738e6677de8e94ef560b55500944db30
minami <minaminaoy@gmail.com>
2022-08-02 05:19:54 +0900

    Add codes

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+library Library {
+    function f(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+}
+
+contract Contract {
+    uint256 c;
+
+    constructor() {
+        c = Library.f(1, 2);
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/Contract.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    function setUp() public {
+        vm.createSelectFork("https://rpc.ankr.com/eth_goerli", 7329883);
+    }
 
-    function testExample() public {
-        assertTrue(true);
+    function test() public {
+        new Contract();
     }
 }
